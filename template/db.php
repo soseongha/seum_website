d8906c450fbb6fd2876df155017dfb43ad509e5f
소성하 <79681915+soseongha@users.noreply.github.com>
2023-02-14 12:18:40 +0900

    delete important information for db

--- template/db.php
+++ template/db.php
@@ -2,7 +2,7 @@
 /*htaccess 등을 이용해서 접근 금지 처리할 것!!!*/
 function db_connect(){
 
-    $conn = mysqli_connect('localhost','dusdlab','dusdlab2021!','dusdlab');/*실서버에서의 정보로 변경 요망!!!*/
+    $conn = mysqli_connect('localhost','server_name','password','server_name');/*실서버에서의 정보로 변경 요망!!!*/
     
     return $conn;
 }
